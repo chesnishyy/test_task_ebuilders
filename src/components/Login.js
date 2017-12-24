3a71b25f24cf3b71e8384fde32dccae9eb9bb27e
Igor Chesnishyy <i.chesnishyy@gmail.com>
2017-12-24 22:44:48 +0200

    Project settings, routing, main components

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+         <h1>Login Page</h1>
+         <h1>Login Page</h1>
+         <h1>Login Page</h1>
+      </div>
+   );
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IndexRoute, Route } from 'react-router';
+
+import App from './components/App';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+
+const routes = () => {
+  return (
+    <Route path='/' component={App}>
+      <IndexRoute component={Register} />
+      <Route path='login' component={Login} />
+      <Route path='home' component={Home} />
+    </Route>
+  );
+};
+
+export default routes;
